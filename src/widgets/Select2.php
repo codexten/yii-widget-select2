dee41e8ec7f2b503e494235746b89641058f4a7f
Jomon Johnson <jomon.entero@gmail.com>
2019-03-06 14:28:34 +0530

    init

---
+++ src/widgets/Select2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 6/3/19
+ * Time: 2:26 PM
+ */
+
+namespace codexten\yii\widgets;
+
+
+class Select2 extends \kartik\select2\Select2
+{
+
+}
