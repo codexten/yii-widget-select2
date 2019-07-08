06d7002996b8a7a450cf679ff98bb20a14bdc6a0
Jomon Johnson <jomon.entero@gmail.com>
2019-07-08 14:47:34 +0530

    country api

--- src/widgets/Select2.php
+++ src/widgets/Select2.php
@@ -9,10 +9,64 @@
 namespace codexten\yii\widgets;
 
 
+use yii\helpers\Url;
+use yii\web\JsExpression;
+
 class Select2 extends \kartik\select2\Select2
 {
-    public $options = [
-        'placeholder' => '',
-    ];
+    public $placeholder = '';
+    public $initValue = '';
+    public $url = false;
+    public $data = null;
+    public $idField = 'id';
+    public $textField = 'name';
 
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->normalize();
+        parent::init();
+    }
+
+    /**
+     * Normalizing configuration
+     */
+    public function normalize()
+    {
+        if ($this->placeholder) {
+            $this->options['placeholder'] = $this->placeholder;
+        }
+        if ($this->initValue) {
+            $this->initValueText = $this->initValue;
+        }
+
+        if ($this->url) {
+            $this->pluginOptions['ajax']['url'] = Url::to($this->url);
+            $this->pluginOptions['ajax']['processResults'] = new JsExpression($this->processResults());
+        }
+    }
+
+    protected function processResults()
+    {
+        return <<<JS
+function (data, params) {
+var arrayLength = data.length;
+var items = [];
+for (var i = 0; i < arrayLength; i++) {
+    var item = data[i];
+  
+    items.push({
+        'id' : item['{$this->idField}'],
+     'text' : item['{$this->textField}'] 
+    });
+}
+    return {
+        results: items
+    };
+}
+JS;
+
+    }
 }
